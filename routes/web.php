8aaa9e9f2b71fa3229f825bd18053de6e385a207
felipeverse <felipealvesrrodrigues@outlook.com>
2022-06-07 11:05:49 -0400

    fix: corrige erro de autenticacao das rotas

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\BuscaCEPController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,5 +12,7 @@ Route::get('cep/{cep}', [BuscaCEPController::class, 'fetch']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\ContatoController::class, 'index'])->name('home');
-Route::resource('contatos', ContatoController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\ContatoController::class, 'index'])->name('home');
+    Route::resource('contatos', ContatoController::class);
+});
